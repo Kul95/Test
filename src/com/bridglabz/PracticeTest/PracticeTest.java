bf39fc0f448de9d6f786fc32fb4620bfba98e371
Kuldeep Kumar <kuldeepkumar17feb@gmail.com>
2023-05-19 21:16:12 +0530

    Added :Copun code

--- src/com/bridglabz/PracticeTest/PracticeTest.java
+++ src/com/bridglabz/PracticeTest/PracticeTest.java
@@ -1,24 +1,22 @@
 package com.bridglabz.PracticeTest;
 import java.util.*;
 import java.util.Scanner;
-
 public class PracticeTest {
     public static void main(String[] args) {
         coupneCode1();
     }
 public static void coupneCode1(){
-    Scanner sc=new Scanner(System.in);
-
-    String str=sc.next().toString();
-    int max=45;
-    int random=(int) (Math.random()*max);
+//    Scanner sc=new Scanner(System.in);
+//    String str=sc.next().toString();
+    char[] ch="123456789 abcdefghijklmnopqrstuvwxyz ABCDEFGHIJKLMNOPQRSTUVWXYZ".toCharArray();
+    int max=4556;
+    int random= (int)(Math.random()*max);
     StringBuffer sb=new StringBuffer();
 		while (random>0)
     {
-        sb.append((random % str.length()));
-        random =random/str.length();
+        sb.append(ch[random % ch.length]);
+        random =random/ch.length;
     }
-
     String couponCode=sb.toString();
 		System.out.println("Coupon Code: "+couponCode);
     }
